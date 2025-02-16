3465175263dfe72edf8377166e785410eb3dd2d6
gbouget <bouget.g@gmail.com>
2025-02-16 18:51:11 +0100

    Initialize Gradle, Kotlin and Spring Boot

---
+++ src/main/kotlin/dev/gbouget/interest_tools/InterestToolsApplication.kt
@@ -0,0 +1,11 @@
+package dev.gbouget.interest_tools
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterestToolsApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterestToolsApplication>(*args)
+}
